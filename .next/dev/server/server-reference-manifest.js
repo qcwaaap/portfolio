80c06bdff2c1617e438fcc7a5e03a7b8239fd5cb
masha <masikemokid@gmail.com>
2026-09-20 01:17:14 -0700

    add gitignore

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"gYCPgS3Fo67BrZA/TGurvpiAftB9hBtisiTU9IXrLmY=\"\n}"
--- .next/dev/static/chunks/_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_0h2if2c._.css","static/chunks/node_modules_next_dist_20wefz_._.js","static/chunks/node_modules_0-xg2aa._.js","static/chunks/src_0wiy6g7._.js","static/chunks/src_components_loader_16g1hyl._.css"],
-    source: "entry"
-});
